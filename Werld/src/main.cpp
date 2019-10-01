db2d2e7046aae8ea1be31a3869b28ebb159ec250
Nick Gridinskiy <nikita.gridinskiy@gmail.com>
2019-09-30 17:46:39 -0700

    Boost working on mac now, just trying out the logging

--- Werld/src/main.cpp
+++ Werld/src/main.cpp
@@ -1,3 +1,4 @@
+#include <boost/log/trivial.hpp>
 #include <VulkanExperiment/experiment.hpp>
 #include <iostream>
 
@@ -5,6 +6,7 @@ int main() {
   // HelloTriangleApplication app;
   auto app = vke::platform::createApplication();
   try {
+    BOOST_LOG_TRIVIAL(info) << "Starting the Werld app... Word...";
     app->run();
   } catch (const std::exception &e) {
     std::cerr << e.what() << std::endl;
